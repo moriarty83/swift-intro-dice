9bd276f0daf63e083790d31991f17245ca2efb9e
moriarty83 <moriarty83@gmail.com>
2021-12-03 21:41:27 -0500

    initial commit

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // to set image literal #imageLiteral()
+
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
 
+    }
+    
 }
 
